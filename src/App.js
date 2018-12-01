0075f3d1515c4f96f4d419cfb18b09ed1ed418f4
Darius Ray <Drayz@users.noreply.github.com>
2018-11-30 23:49:04 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,64 +1,101 @@
-import React, { Component } from 'react';
-import menu from './menu.png';
+import React, { Component } from "react";
+import menu from "./menu.png";
 import MapContent from "./components/MapContent";
 import Settings from "./components/Settings";
 import "./App.css";
 
+//Information from my Foursquare API that get passed into the `let` url
 const client = "XINTEH3WSKC5FJ2WPHER3EDB2SBUGZTQ4UQFO52TVCGTRMR2";
 const secret = "I5XXOLB2Y41MRYG4UXS22V2V5VLM43GOTCU12QEBLUMOVKSH";
 const version = "20182611";
 
 class App extends Component {
   state = {
-    lat: 40.750580,
+    lat: 40.75058,
     lon: -73.993584,
     zoom: 13,
     allPlaces: [],
-    filteredPlaces: null
+    query: "",
+    filteredPlaces: [],
+    isOpen: false
   };
 
+  //credit by Doug Brown's FEND7 Neighborhood Map
   componentDidMount = () => {
-    let url = `https://api.foursquare.com/v2/venues/search?client_id=${client}&client_secret=${secret}&v=${version}&radius=5000&ll=${this.state.lat},${this.state.lon}&intent=browse&query=Sushi`;
+    let link = `https://api.foursquare.com/v2/venues/search?client_id=${client}&client_secret=${secret}&v=${version}&radius=5000&ll=${
+      this.state.lat
+    },${this.state.lon}&intent=browse&query=Sushi`;
     let headers = new Headers();
-    let request = new Request(url, {
-     method: 'GET',
-     headers
-   });
-   fetch(request)
-     .then(response => response.json())
-     .then(json => {
-       const allPlaces = json.response.venues;
-       this.setState({
-         allPlaces,
-         filtered: this.filterLocals(allPlaces, "")
-       });
-     })
-     .catch(error => {
-       alert("FourSquare data was not able to be received");
-     });
- }
+    let request = new Request(link, {
+      method: "GET",
+      headers
+    });
+    fetch(request)
+      .then(response => response.json())
+      .then(json => {
+        const allPlaces = json.response.venues;
+        this.setState(
+          {
+            allPlaces: allPlaces,
+            filteredPlaces: allPlaces
+          },
+          () => console.log(this.state)
+        );
+      })
+      .catch(error => {
+        alert("FourSquare data was not able to be received");
+      });
+  };
 
- filterLocals = (venues, query) => {
+  handleQuery = query => {
+    this.setState(
+      {
+        query
+      },
+      () => this.filterLocals(query)
+    );
+  };
+
+  filterLocals = query => {
     // Filter locations to match query string
-    return venues.filter(venue => venue.name.toLowerCase().includes(query.toLowerCase()));
-  }
+    const newPlaces = this.state.allPlaces.filter(venue =>
+      venue.name.toLowerCase().includes(query.toLowerCase())
+    );
+    this.setState({ filteredPlaces: newPlaces });
+  };
+
+  handleMapClick = e => {
+    console.log(e);
+  };
+
+  handleMenu = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <div>
-            <img src={menu} alt="menulogo" />
+        <header className="App-header" >
+          <div onClick={this.handleMenu}>
+            <img src={menu} alt="menulogo" roles="menu"/>
           </div>
           <h4>Darius's Sushi Spots</h4>
         </header>
         <div>
-        <Settings locations={this.state.filteredPlaces}/>
+          {this.state.isOpen && (
+            <Settings
+              locations={this.state.filteredPlaces}
+              query={this.state.query}
+              handleQuery={this.handleQuery}
+            />
+          )}
         </div>
         <MapContent
-          locations={this.state.allPlaces}
-          venues={this.state.filteredPlaces}
-           />
+          filteredPlaces={this.state.filteredPlaces}
+          onMapClicked={this.handleMapClick}
+        />
       </div>
     );
   }
